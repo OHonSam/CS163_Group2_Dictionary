d92359345d63792f1005f58f431798a3bd092802
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-06-27 15:45:00 +0700

    add std:: in HashTable

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -1,17 +1,10 @@
-#include <fstream>
-#include <vector>
-#include <string.h>
 #include <HashTable.hpp>
 
-HashTable::string searchDef(const string& word) {
-    HashTable::numWords = HashTable::hash(word);
-    for (int i = 0; i < HashTable::buckets[HashTable::numWords].size(); i++)
-    {
-        if (buckets[HashTable::numWords][i].first == word) return buckets[HashTable::numWords][i].second;
-    }
+std::string HashTable::searchDef(const std::string &word)
+{
+    numWords=hash(word);
+    for (int i=0;i<buckets[numWords].size();i++)
+        if (buckets[numWords][i].first==word) 
+            return buckets[numWords][i].second;
     return "";
-}
-
-HashTable::insert(const string& word, const string& def) {
-    
 }
