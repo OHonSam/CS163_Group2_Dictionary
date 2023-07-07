7e46fe8a7c79e09838b05f5fe89043224f8e62b5
hhplinh <huynhhaphuonglinh@gmail.com>
2023-07-07 08:10:44 +0700

    Add Insertion to BTree

--- include/BTree.hpp
+++ include/BTree.hpp
@@ -1,13 +1,39 @@
 #ifndef BTREE_HPP
 #define BTREE_HPP
-class BTree
-{
-	BTree** root;
-	int* children;
 
+class TreeNode
+{
+	int *keys;
+	int minimum_degree;
+	TreeNode **Children;
+	int order;
+	bool leaf;
 
 public:
-	BTree();
+	TreeNode(int temp, bool bool_leaf);
+
+	void insertNonFull(int k);
+	void splitChild(int i, TreeNode *y);
+	void traverse();
+
+	TreeNode *search(int k);
+
+	friend class BTree;
+};
+
+class BTree
+{
+	TreeNode *root;
+	int minimum_degree;
+
+public:
+	BTree(int temp);
+
+	void traverse();
+
+	TreeNode *search(int k);
+	void insert(int k);
+
 	~BTree();
 };
 #endif
