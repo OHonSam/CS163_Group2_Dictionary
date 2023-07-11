8c6571de89739137a8726412b2579a626c979083
hhplinh <huynhhaphuonglinh@gmail.com>
2023-07-12 02:19:08 +0700

    Testing TST

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
-// #include "Trie.hpp"
+// #include "TST.hpp"
 
 // int main()
 // {
-//     Trie trie;
-//     trie.insert("hello");
-//     trie.insert("hi");
-//     trie.insert("hey");
-//     std::vector<std::string> s = trie.searchPrefix("he");
-
-//     for (auto &i : s)
-//         std::cout << i << std::endl;
-
-//     trie.save("test\\output.bin");    
+//     TST tst;
+//     tst.insert("hello");
+//     tst.insert("hi");
+//     tst.insert("hey");
     
+//     tst.save("test\\output.bin");
+
 //     return 0;
 // }
+// // #include "Trie.hpp"
+
+// // int main()
+// // {
+// //     Trie trie;
+// //     trie.insert("hello");
+// //     trie.insert("hi");
+// //     trie.insert("hey");
+// //     std::vector<std::string> s = trie.searchPrefix("he");
+
+// //     for (auto &i : s)
+// //         std::cout << i << std::endl;
+
+// //     trie.save("test\\output.bin");    
+    
+// //     return 0;
+// // }
