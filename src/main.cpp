c5eeab5254448ddf7b060d851097e5487a248874
OHonSam <ohsam22@apcs.fitus.edu.vn>
2023-07-16 14:45:02 +0700

    More test

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,15 @@ int main(){
     std::cout << POS::getType("Noun") << std::endl;
     std::cout << POS::getType("Adjective") << std::endl;
     std::cout << POS::getType("Interjection") << std::endl;
-    std::cout << POS::getTypeString(0) << std::endl;
-    std::cout << POS::getTypeString(1) << std::endl;
-    std::cout << POS::getTypeString(7) << std::endl;
+
     word->word = "hello";
     word->def[0].push_back("xin chao");
+    word->def[0].push_back("loi chao");
+    word->def[0].push_back("loi chao hoi");
     word->def[1].push_back("chao");
+    word->def[1].push_back("chao hoi");
     word->def[7].push_back("thu hut");
+    std::cout<<POS::getTypeString(POS::getType(word->def[0][0]));
     HashTable h;
     h.insert(word);
     h.displayDef(word->word);
