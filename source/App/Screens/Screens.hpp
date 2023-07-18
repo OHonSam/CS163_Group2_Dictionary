fb77ca8800ebbf921394de6b2590c9b20ff55cbd
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-07-18 22:44:35 +0700

    define Screen

--- source/App/Screens/Screens.hpp
+++ source/App/Screens/Screens.hpp
@@ -1,9 +1,22 @@
 #ifndef SCREENS_HPP
 #define SCREENS_HPP
 
+#include <Dict.hpp>
+
 class Screen
 {
+protected:
+    bool isEnd;
+    Dict* dict;
 
+public:
+    Screen(Dict* dict)
+    {
+        this->dict = dict;
+        isEnd = false;
+    }
+
+    virtual void render() = 0;
 };
 
 #endif // SCREENS_HPP
