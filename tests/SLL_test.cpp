ea945a40e25f876e58dfd21f37a7bb83639721e5
OHonSam <ohsam22@apcs.fitus.edu.vn>
2023-07-18 22:02:18 +0700

    add TEST(SLL_test,ImportAndSave)

--- tests/SLL_test.cpp
+++ tests/SLL_test.cpp
@@ -44,6 +44,21 @@ TEST(SLL_test, clearSLL)
         EXPECT_FALSE(sll.find(word[i]));
 }
 
+TEST(SLL_test, ImportAndSave)
+{
+    SLL<string> sll;
+    for(int i=0; i<N; i++)
+        sll.push(word[i]);
+    ASSERT_TRUE(sll.saveSLLStr("SLL.bin"));
+    sll.clearSLL();
+    EXPECT_FALSE(sll.find(word[0]));
+
+    
+    ASSERT_TRUE(sll.importSLLStr("SLL.bin"));
+    for(int i=0; i<N; i++)
+        EXPECT_TRUE(sll.find(word[i]));
+    ASSERT_TRUE(sll.clearHistory("SLL.bin"));
+}
 // TEST(SLL_test, ImportAndSave)
 // {
 //     SLL<string> sll;
