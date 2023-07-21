bb1efd003bacbdf169fb751e48e3f57785d74a88
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-07-21 19:11:39 +0700

    add Dict_test.addWord (test FAIL)

--- tests/Dict_test.cpp
+++ tests/Dict_test.cpp
@@ -26,7 +26,7 @@ const vector<string> def={
     "One of the compartments in a church which are separated by low partitions  and have long seats upon which several persons may sit; -- sometimes called slip. Pews were originally made square but are now usually long and narrow."
 };
 
-TEST(Dict, ReadCsvAndSearch)
+TEST(Dict_test, ReadCsvAndSearch)
 {
     Dict dict;
 
@@ -38,4 +38,18 @@ TEST(Dict, ReadCsvAndSearch)
         ASSERT_FALSE(w->def[POS::getIndex(type[i])].empty());
         EXPECT_TRUE(w->def[POS::getIndex(type[i])][0]==def[i]);
     }
+}
+
+TEST(Dict_test, addWord)
+{
+    Dict dict;
+    Word* w=new Word("abc",POS::Noun,"abc_def");
+    EXPECT_TRUE(dict.searchDef("abc")==nullptr);
+    dict.addWord(w);
+    Word* res=dict.searchDef("abc");
+    ASSERT_TRUE(res!=nullptr);
+    EXPECT_TRUE(res->word=="abc");
+    ASSERT_FALSE(res->def[POS::getIndex(POS::Noun)].empty());
+    EXPECT_TRUE(res->def[POS::getIndex(POS::Noun)][0]=="abc_def");
+    delete w;
 }
