70534d89063c265dd74dc1af384d991db11a9660
OHonSam <ohsam22@apcs.fitus.edu.vn>
2023-07-23 13:43:13 +0700

    fix TST::import()/fix xstring errors

--- source/DataStructure/TST/TST.cpp
+++ source/DataStructure/TST/TST.cpp
@@ -221,12 +221,12 @@ bool TST::import(const std::string &path)
     {
         std::cout << "File not found\n";
         return false;
+    }
     if(file.peek() == std::ifstream::traits_type::eof())
         return true;
     import(root, file);
     file.close();
     return true;
-    }
 }
 
 bool TST::save(const std::string &path)
