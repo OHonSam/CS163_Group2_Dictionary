500c068abbd7f8d503eda44b8b7ec659c3c4376f
hhplinh <huynhhaphuonglinh@gmail.com>
2023-07-31 01:35:08 +0700

    test change to txt, error in import trie

--- source/main.cpp
+++ source/main.cpp
@@ -8,33 +8,55 @@
 //     return 0;
 // }
 
-#include "../source/DataStructure/TST/TST.hpp"
+// #include "../source/DataStructure/trie/trie.hpp"
 #include "../Libraries.hpp"
+#include "../source/DataStructure/Trie/Trie.hpp"
+
+// int main()
+// {
+//     trie trie;
+//     trie.insert("hello");
+
+//     trie.saveTXT("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/trie/trie.txt");
+//     std::cerr << "nice\n";
+
+//     std::vector<std::string> v=trie.traverse();
+//     for(const std::string& s: v)
+//         std::cout<<s<<'\n';
+
+//     // trie.import("test.bin");
+//     trie.clear();
+//     std::cerr << "Tree: " << trie.treeExists();
+//     trie.importTXT("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/trie/trie.txt");
+//     // std::cerr << "out import\n";
+//     // std::vector<std::string> v = trie.traverse();
+//     std::cerr << "after import: " << trie.treeExists() << '\n';
+//     v = trie.traverse();
+//     for (const std::string &s : v)
+//     {
+//         std::cout << s << std::endl;
+//     }
+
+//     std::cerr << "end\n";
+
+//     return 0;
+// }
+
 
 int main()
 {
-    TST tst;
-    tst.insert("hello");
+    Trie trie;
+    trie.insert("hello");
 
-    tst.saveTXT("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/TST/TST.txt");
+    trie.save("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/trie/trie.txt");
     std::cerr << "nice\n";
 
-    std::vector<std::string> v=tst.traverse();
-    for(const std::string& s: v)
-        std::cout<<s<<'\n';
-
-    // tst.import("test.bin");
-    tst.clear();
-    std::cerr << "Tree: " << tst.treeExists();
-    tst.importTXT("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/TST/TST.txt");
+    // trie.import("test.bin");
+    trie.clear();
+    trie.import("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/trie/trie.txt");
     // std::cerr << "out import\n";
-    // std::vector<std::string> v = tst.traverse();
-    std::cerr << "after import: " << tst.treeExists() << '\n';
-    v = tst.traverse();
-    for (const std::string &s : v)
-    {
-        std::cout << s << std::endl;
-    }
+    // std::vector<std::string> v = trie.traverse();
+    // std::cerr << "after import: " << trie.treeExists() << '\n';
 
     std::cerr << "end\n";
 
