3d4222f6df6e9ef97034b8c24dde338c845a5f14
hhplinh <huynhhaphuonglinh@gmail.com>
2023-07-31 11:30:14 +0700

    test

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,77 @@
-#include <App.hpp>
-using namespace std;
+// #include <App.hpp>
+// using namespace std;
+
+// int main()
+// {
+//     // App app;
+//     // app.run();
+//     // return 0;
+
+    
+// }
+
+
+// #include <App.hpp>
+// using namespace std;
+
+// int main()
+// {
+//     App app;
+//     app.run();
+//     return 0;
+// }
+
+#include "../source/DataStructure/TST/TST.hpp"
+#include "../Libraries.hpp"
+// #include "../source/DataStructure/Trie/Trie.hpp"
 
 int main()
 {
-    App app;
-    app.run();
+    TST tst;
+    tst.insert("hello");
+
+    tst.saveTXT("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/tst/tst.txt");
+    std::cerr << "nice\n";
+
+    std::vector<std::string> v=tst.traverse();
+    for(const std::string& s: v)
+        std::cout<<s<<'\n';
+
+    // tst.import("test.bin");
+    tst.clear();
+    std::cerr << "Tree: " << tst.treeExists();
+    tst.importTXT("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/tst/tst.txt");
+    // std::cerr << "out import\n";
+    // std::vector<std::string> v = tst.traverse();
+    std::cerr << "after import: " << tst.treeExists() << '\n';
+    v = tst.traverse();
+    for (const std::string &s : v)
+    {
+        std::cout << s << std::endl;
+    }
+
+    std::cerr << "end\n";
+
     return 0;
 }
+
+
+// int main()
+// {
+//     Trie trie;
+//     trie.insert("hello");
+
+//     trie.save("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/trie/trie.txt");
+//     std::cerr << "nice\n";
+
+//     // trie.import("test.bin");
+//     trie.clear();
+//     trie.import("C:/Users/huynh/OneDrive - VNU-HCMUS/KHTN (do not delete)/Year 1/Semester 3/CS163 Data structure/lab project cs163/CS163_Group2_Dictionary/source/DataStructure/trie/trie.txt");
+//     // std::cerr << "out import\n";
+//     // std::vector<std::string> v = trie.traverse();
+//     // std::cerr << "after import: " << trie.treeExists() << '\n';
+
+//     std::cerr << "end\n";
+
+//     return 0;
+// }
