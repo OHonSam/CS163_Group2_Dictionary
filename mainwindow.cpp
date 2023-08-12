8596f12543d17c25099966f6f1fdd085bb85ca00
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-08-12 12:03:41 +0700

    load font from resources

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,40 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QFontDatabase>
+#include <QDir>
+#include <QStringList>
+#include <QFile>
+#include <QByteArray>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Load fonts from the resource folder
+
+    // 1. Audiowide
+    QDir fontDir(":/font/font/Audiowide/");
+    QStringList fontFiles = fontDir.entryList(QStringList() << "*.ttf", QDir::Files);
+    foreach (const QString &fontFile, fontFiles) {
+        QFile fontResource(":/font/font/Audiowide/" + fontFile);
+        fontResource.open(QIODevice::ReadOnly);
+        QByteArray fontData = fontResource.readAll();
+        QFontDatabase::addApplicationFontFromData(fontData);
+    }
+
+    // 2. Comfortaa
+    fontDir = QDir(":/font/font/Comfortaa/static/");
+    fontFiles = fontDir.entryList(QStringList() << "*.ttf", QDir::Files);
+    foreach (const QString &fontFile, fontFiles) {
+        QFile fontResource(":/font/font/Comfortaa/static/" + fontFile);
+        fontResource.open(QIODevice::ReadOnly);
+        QByteArray fontData = fontResource.readAll();
+        QFontDatabase::addApplicationFontFromData(fontData);
+    }
+
 }
 
 MainWindow::~MainWindow()
