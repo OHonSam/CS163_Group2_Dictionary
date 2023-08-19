aa5ec548bafe1749eb0f65f391c7d40d1ac38689
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-08-19 22:37:48 +0700

    actually save new def

--- editscreen.cpp
+++ editscreen.cpp
@@ -43,7 +43,17 @@ void EditScreen::on_pushButton_save_clicked()
         "The old definition will not be able to recovered.\nDo you want to save?",
                                 QMessageBox::Yes|QMessageBox::No);
     if(rep==QMessageBox::Yes){
-
+        std::vector<std::string> output;
+        QStringList lines=ui->plainTextEdit_def->toPlainText().split('\n');
+        foreach(QString line,lines){
+            QString trimmedLine = line.trimmed();
+            if (!trimmedLine.isEmpty()) {
+                QString value = trimmedLine.mid(2);  // Assuming the "- " prefix should be removed
+                output.push_back(value.toStdString());
+            }
+        }
+        curWord->def[ui->comboBox_POS->currentIndex()]=output;
+        on_comboBox_POS_currentIndexChanged(ui->comboBox_POS->currentIndex());
     }
 }
 
