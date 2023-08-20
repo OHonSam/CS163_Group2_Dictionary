d594a0605bb3260493fe0aeb2b72908f75ada646
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-08-20 20:43:55 +0700

    enable new quiz button in quiz.ui

--- quiz.cpp
+++ quiz.cpp
@@ -6,10 +6,32 @@ Quiz::Quiz(Dict *dict, QWidget *parent) :
     ui(new Ui::Quiz)
 {
     ui->setupUi(this);
+    on_pushButton_newQuiz_clicked();
 }
 
 Quiz::~Quiz()
 {
     delete ui;
+}
+
+
+void Quiz::on_pushButton_newQuiz_clicked()
+{
+    correctId=rand()%NUMC;
+    words=dict->getMultiChoices(NUMC);
+    if(ui->comboBox->currentIndex()==int(MC::Def)){
+        ui->label_ques->setText(QString::fromStdString(words[correctId]->getRandDef()));
+        ui->pushButton_c1->setText(QString::fromStdString(words[0]->word));
+        ui->pushButton_c2->setText(QString::fromStdString(words[1]->word));
+        ui->pushButton_c3->setText(QString::fromStdString(words[2]->word));
+        ui->pushButton_c4->setText(QString::fromStdString(words[3]->word));
+    }
+    else{
+        ui->label_ques->setText(QString::fromStdString(words[correctId]->word));
+        ui->pushButton_c1->setText(QString::fromStdString(words[0]->getRandDef()));
+        ui->pushButton_c2->setText(QString::fromStdString(words[1]->getRandDef()));
+        ui->pushButton_c3->setText(QString::fromStdString(words[2]->getRandDef()));
+        ui->pushButton_c4->setText(QString::fromStdString(words[3]->getRandDef()));
+    }
 }
 
--- quiz.h
+++ quiz.h
@@ -7,6 +7,13 @@ namespace Ui {
 class Quiz;
 }
 
+namespace MC{
+enum Type{
+    Def, // 1 def 4 words
+    Word, // 1 word 4 defs
+};
+}
+
 class Quiz : public MyScreen
 {
     Q_OBJECT
@@ -15,8 +22,16 @@ public:
     explicit Quiz(Dict *dict, QWidget *parent = nullptr);
     ~Quiz();
 
+private slots:
+    void on_pushButton_newQuiz_clicked();
+
 private:
     Ui::Quiz *ui;
+
+    const int NUMC=4;
+
+    int correctId;
+    std::vector<Word*> words;
 };
 
 #endif // QUIZ_H
