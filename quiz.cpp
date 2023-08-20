8b36d0a0d37c3d1858305ab7cbf1a3c7d028dfdc
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-08-20 21:08:24 +0700

    handle user choice in quiz

--- quiz.cpp
+++ quiz.cpp
@@ -21,17 +21,53 @@ void Quiz::on_pushButton_newQuiz_clicked()
     words=dict->getMultiChoices(NUMC);
     if(ui->comboBox->currentIndex()==int(MC::Def)){
         ui->label_ques->setText(QString::fromStdString(words[correctId]->getRandDef()));
-        ui->pushButton_c1->setText(QString::fromStdString(words[0]->word));
-        ui->pushButton_c2->setText(QString::fromStdString(words[1]->word));
-        ui->pushButton_c3->setText(QString::fromStdString(words[2]->word));
-        ui->pushButton_c4->setText(QString::fromStdString(words[3]->word));
+        for(int i=0; i<NUMC; i++)
+            getButton(i)->setText(QString::fromStdString(words[i]->word));
     }
     else{
         ui->label_ques->setText(QString::fromStdString(words[correctId]->word));
-        ui->pushButton_c1->setText(QString::fromStdString(words[0]->getRandDef()));
-        ui->pushButton_c2->setText(QString::fromStdString(words[1]->getRandDef()));
-        ui->pushButton_c3->setText(QString::fromStdString(words[2]->getRandDef()));
-        ui->pushButton_c4->setText(QString::fromStdString(words[3]->getRandDef()));
+        for(int i=0; i<NUMC; i++)
+            getButton(i)->setText(QString::fromStdString(words[i]->getRandDef()));
     }
+
+    for(int i=0; i<NUMC; i++) getButton(i)->setStyleSheet(normalStyle);
+}
+
+
+void Quiz::on_pushButton_c0_clicked()
+{
+    considerChoice(0);
+}
+
+QPushButton* Quiz::getButton(int id){
+    switch(id){
+    case 0: return ui->pushButton_c0; break;
+    case 1: return ui->pushButton_c1; break;
+    case 2: return ui->pushButton_c2; break;
+    case 3: return ui->pushButton_c3; break;
+    }
+    return nullptr;
+}
+
+void Quiz::considerChoice(int id){
+    getButton(correctId)->setStyleSheet(correctStyle);
+    if(id!=correctId) getButton(id)->setStyleSheet(incorrectStyle);
+}
+
+void Quiz::on_pushButton_c1_clicked()
+{
+    considerChoice(1);
+}
+
+
+void Quiz::on_pushButton_c2_clicked()
+{
+    considerChoice(2);
+}
+
+
+void Quiz::on_pushButton_c3_clicked()
+{
+    considerChoice(3);
 }
 
--- quiz.h
+++ quiz.h
@@ -3,6 +3,8 @@
 
 #include "myscreen.h"
 
+#include <QPushButton>
+
 namespace Ui {
 class Quiz;
 }
@@ -25,13 +27,57 @@ public:
 private slots:
     void on_pushButton_newQuiz_clicked();
 
-private:
-    Ui::Quiz *ui;
+    void on_pushButton_c0_clicked();
 
+    void on_pushButton_c1_clicked();
+
+    void on_pushButton_c2_clicked();
+
+    void on_pushButton_c3_clicked();
+
+private:
     const int NUMC=4;
+
+    QString normalStyle="padding: 20px;"
+                           "border-width : 2px;"
+                           "border-style:solid;"
+                           "border-radius: 20px;"
+                           "min-height: 20px;"
+                           "min-width: 20px;"
+                           "background-color: rgb(233, 240, 250);"
+                           "border-color: rgb(99, 112, 243);"
+                           "font: 13pt \"Comfortaa\";"
+                           "color: rgb(56, 64, 139);";
+
+    QString correctStyle="padding: 20px;"
+                        "border-width : 2px;"
+                        "border-style:solid;"
+                        "border-radius: 20px;"
+                        "min-height: 20px;"
+                        "min-width: 20px;"
+                        "background-color: green;"
+                        "border-color: rgb(99, 112, 243);"
+                        "font: 13pt \"Comfortaa\";"
+                        "color: white;";
+
+    QString incorrectStyle="padding: 20px;"
+                           "border-width : 2px;"
+                           "border-style:solid;"
+                           "border-radius: 20px;"
+                           "min-height: 20px;"
+                           "min-width: 20px;"
+                           "background-color: red;"
+                           "border-color: rgb(99, 112, 243);"
+                           "font: 13pt \"Comfortaa\";"
+                           "color: white;";
+
+    Ui::Quiz *ui;
 
     int correctId;
     std::vector<Word*> words;
+
+    QPushButton* getButton(int id);
+    void considerChoice(int id);
 };
 
 #endif // QUIZ_H
